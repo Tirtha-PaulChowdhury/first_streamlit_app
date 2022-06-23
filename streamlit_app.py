2d4b1962b8237379f121368e10b6eff51497de73
Tirtha-PaulChowdhury <62827036+Tirtha-PaulChowdhury@users.noreply.github.com>
2022-06-24 01:42:20 +0530

    Update streamlit_app.py
    imported pandas, used pandas dataframe and read_csv to get csv file containing fruit list into the application

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
+#Library imports
 import streamlit
+import pandas
 
+#adding web-app title
 streamlit.title('My Mom\'s New Healthy Diner')
 
+#adding the menu
 streamlit.header('Breakfast Favourites')
 streamlit.text('🍜Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍱🍹Kale, Spinach & Rocket Smoothie')
@@ -10,3 +14,7 @@ streamlit.text('🍐🍞Avocado Toast')
 
 
 streamlit.header('🍌🍉Build Your own Fruit Smoothie🍓🍍')
+
+#using pandas dataframe to store all the fruits from a file in S3
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
