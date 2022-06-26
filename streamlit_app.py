88a13f96881361d0b49c77d5d6e8c78dde326bb3
Tirtha Paul Chowdhury <62827036+Tirtha-PaulChowdhury@users.noreply.github.com>
2022-06-26 23:21:19 +0530

    Raw JSON remove
    Removing the raw JSON output.

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,6 @@ streamlit.dataframe(fruits_to_show)
 #checking if fruityvice api is working fine or not
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())  #prints the raw JSON data
 
 # Flattening the JSON response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
