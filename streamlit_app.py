d8bb307c98e807896055af1619b242b3ff7d58c0
Tirtha Paul Chowdhury <62827036+Tirtha-PaulChowdhury@users.noreply.github.com>
2022-06-26 23:18:16 +0530

    fetching the result
    fetching the result instead of status code

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,9 +33,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)   
 
 
+
 #checking if fruityvice api is working fine or not
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 
